423428095607db53189feb710e0e4e649a692a79
Tungsten842 <886724vf@anonaddy.me>
2024-05-21 15:41:18 +0200

    Rewrite comments with appendChild

--- article.js
+++ article.js
@@ -13,18 +13,30 @@ async function load_article() {
   console.log(data);
 
   content = document.querySelector(".content");
-  content.innerHTML += `
+  const html = `
     <h2 id=a_title>${article.title}</h2>
     <div id=a_author>${article.author}</div>
     <img id=a_img src="data:image/jpg;base64,${article.image}">
     <div id=a_text>${article.text}</div>
   `;
-  content.innerHTML += "<div id=a_comments></div>"
+  content.insertAdjacentHTML("afterbegin", html);
+
   comments = document.querySelector("#a_comments");
 
   for (const comm of article.comments) {
-    comments.innerHTML += `<div>${comm.name}</div>`;
-    comments.innerHTML += `<div>${comm.text}</div>`;
+    const single_comment = document.createElement('div');
+    single_comment.className = 'single_comment';
+    comments.appendChild(single_comment);
+
+    const comment_name = document.createElement('div');
+    comment_name.className = 'comment_name';
+    comment_name.textContent = comm.name;
+    single_comment.appendChild(comment_name);
+
+    const comment_text = document.createElement('div');
+    comment_text.className = 'comment_text';
+    comment_text.textContent = comm.text;
+    single_comment.appendChild(comment_text);
   }
 }
 load_article();
--- article.php
+++ article.php
@@ -2,4 +2,6 @@
 <script src="article.js" defer></script>
 <link rel="stylesheet" href="article.css">
 
+<div id=a_comments></div>
+
 <?php require("footer.html"); ?>
